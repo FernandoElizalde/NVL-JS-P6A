956ed41c5001edfd1d2bef0b4b60cd08f6097329
FernandoElizalde <tonimota1970@gmail.com>
2019-01-03 09:28:01 +0100

    Primer Commit Js

---
+++ NVL-JS-P6A.js
@@ -0,0 +1,7 @@
+<script>
+var mi_nombre=["Fernando","Elizalde","Costalago"];
+var mi_edad=47;
+var dias_semana=["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado","Domingo"];
+var valor_gravedad=9,80665 m_s²;
+var impares=[1,3,5];
+</script>
