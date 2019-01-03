595abb8895a3811f7384bf0e25c5f8f9163024f1
FernandoElizalde <tonimota1970@gmail.com>
2019-01-03 20:35:44 +0100

    Quinto Commit JS

---
+++ NVL-JS-P10B.js
@@ -0,0 +1,11 @@
+var impares=[];
+var pares=[];
+var numero=Math.floor((Math.random()*100)+1);
+for(var i=0; i<50; i++){
+	 return numero;	
+}
+if (numero%2==0) {
+	pares.push(numero);
+else
+	impares.push(numero);
+}
