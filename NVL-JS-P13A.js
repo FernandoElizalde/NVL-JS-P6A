76985d749b6adc23aee72373b9c4c78389ec075e
FernandoElizalde <tonimota1970@gmail.com>
2019-01-04 09:16:27 +0100

    Décimo Commit JS

--- NVL-JS-P13A.js
+++ NVL-JS-P13A.js
@@ -1,14 +1,17 @@
 var primos=[];
 var n=10;
-var i= 0;
+var i=0;
+var a=0;
 function primos(n){
 	do{
 		if(i==2){
 			push.primos(i);
+			a++;
 		}
 		if(i>2&&i%2!=0){
 			push.primos(i);
+			a++;
 		}
 	 i++;	
-	}while(i<n);
+	}while(a<n);
 }
