b7f962121c429b57b00336e1042574f737c98fbd
FernandoElizalde <tonimota1970@gmail.com>
2019-01-04 07:48:19 +0100

    Sexto Commit JS

---
+++ NVL-JS-P12B.js
@@ -0,0 +1,12 @@
+var impares=[];
+var pares=[];
+function par_impar(numero_aleatorio) {
+	if (numero_aleatorio%2==0) {
+		pares.push(numero_aleatorio);
+	} else { impares.push(numero_aleatorio);}
+// body...	}
+var numero_aleatorio= Math.floor((Math.random()*100)+1);
+for(var i=0; i<50; i++){
+	return numero_aleatorio;	
+	par_impar(numero_aleatorio);
+}
