3c071411a1fd183fd7b17c9d8a6487a3da604e65
FernandoElizalde <tonimota1970@gmail.com>
2019-01-04 10:27:34 +0100

    Undécimo Commit JS

---
+++ NVL-JS-P13B.js
@@ -0,0 +1,13 @@
+var palabras="";
+var longitud==alert(palabras.length);
+function palindromos(palabras){
+	var i=0;
+	do{
+		var caracter1=palabras[i];
+		var caracter2=palabras[longitud-(i+1)];
+		i++;
+	}while(caracter1=caracter2)
+	if(i=(longitud-1)/2){
+		return palídromo;
+	}
+}	
